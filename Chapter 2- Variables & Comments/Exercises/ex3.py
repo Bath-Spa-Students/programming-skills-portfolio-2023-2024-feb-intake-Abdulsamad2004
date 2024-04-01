d32020acbddfb6a11ce6ab8905d1e95baea1bac1
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-01 22:37:03 +0400

    exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/ex3.py
@@ -0,0 +1,10 @@
+# Define the person's name with whitespace characters
+person_name = "\t  Abdul Samad \n"
+
+# Print the name once to display the whitespace around the name
+print("Name with whitespace:", person_name)
+
+# Print the name using lstrip(), rstrip(), and strip() functions
+print("Name using lstrip():", person_name.lstrip())
+print("Name using rstrip():", person_name.rstrip())
+print("Name using strip():", person_name.strip())
