958554479ce409368ad2e9199ea857a3845466fe
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-01 23:08:09 +0400

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/ex3.py
@@ -0,0 +1,6 @@
+# Define a list of motorcycles
+motorcycles = ["Honda", "Yamaha", "hayaboosa", "Kawasaki", "Ducati"]
+
+# Print statements about each motorcycle
+for brand in motorcycles:
+    print(f"I would like to own a {brand} motorcycle.")
