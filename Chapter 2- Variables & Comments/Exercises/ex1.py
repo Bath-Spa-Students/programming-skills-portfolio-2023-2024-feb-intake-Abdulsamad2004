6226b4aad4bed75893c63ed67e0a0707fa226c51
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-01 22:30:56 +0400

    exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/ex1.py
@@ -0,0 +1,11 @@
+# Assigning a message to a variable
+message = "Hello, BathSpa"
+
+# Printing the initial message
+print("Initial message:", message)
+
+# Changing the value of the variable to a new message
+message = "Welcome, BathSpa"
+
+# Printing the new message
+print("New message:", message)
