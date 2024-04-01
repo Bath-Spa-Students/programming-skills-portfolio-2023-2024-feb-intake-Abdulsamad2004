f8ddfb6fffc0c617fd26800322d52ffeabfe4e4d
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-01 22:56:38 +0400

    exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/ex5.py
@@ -0,0 +1,13 @@
+# Define the price of each USB stick and the total budget
+usb_price = 6
+total_budget = 50
+
+# Calculate how many USB sticks she can buy
+num_usb_sticks = total_budget // usb_price
+
+# Calculate the remaining amount of money after buying USB sticks
+remaining_money = total_budget % usb_price
+
+# Print the results
+print(" Abdul Rehman can buy", num_usb_sticks, "USB sticks.")
+print(" Abdul Rehman will have £", remaining_money, "  What Abdul Rehman is left after buying the USB sticks.")
