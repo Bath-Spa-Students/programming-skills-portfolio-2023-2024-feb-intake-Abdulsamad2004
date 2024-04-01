5ccc16aa8dfd8c4d1d766415bea3c8d2fea1ef5b
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-01 23:04:17 +0400

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/ex2.py
@@ -0,0 +1,6 @@
+# Define the list of names
+names = ["Taha", "irtiza", " Abdul Rehman", "kamran", "ajnas"]
+
+# Print a personalized message to each person
+for name in names:
+    print(f"Hello, {name}! Good luck with your assignments.")
