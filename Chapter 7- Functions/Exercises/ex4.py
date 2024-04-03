7346963b11a2b6367a6c5b97a885b0b2af3f4fed
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:32:07 +0400

    Create ex4.py

---
+++ Chapter 7- Functions/Exercises/ex4.py
@@ -0,0 +1,11 @@
+def make_shirt(size='large', message='I love Python'):
+    print(f"Making a {size} shirt with the message: '{message}'")
+
+# Making a large shirt with the default message
+make_shirt()
+
+# Making a medium shirt with the default message
+make_shirt(size='medium', message='computer science is mindblowing')
+
+# Making a shirt of any size with a different message
+make_shirt(size='small', message='Coding is awesome!')
