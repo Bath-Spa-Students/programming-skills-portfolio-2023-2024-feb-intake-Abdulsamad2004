de2b5065442b41503a2425a76e9e8032047300cc
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:30:49 +0400

    Create ex2.py

---
+++ Chapter 4- Control Flow/Exercises/ex2.py
@@ -0,0 +1,8 @@
+# Define the alien's color
+alien_color = 'blue'
+
+# Check if the alien's color is green
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting down a green alien.")
+else:
+    print("Congratulations! You just earned 10 points for shooting down an alien of a different color.")
