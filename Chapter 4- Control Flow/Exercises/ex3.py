d29b48526b1ff6a73e2b9cad0b14d2ab1c46c435
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:30:45 +0400

    Create ex3.py

---
+++ Chapter 4- Control Flow/Exercises/ex3.py
@@ -0,0 +1,10 @@
+# Define the alien's color
+alien_color = 'green'
+
+# Check the alien's color and print the appropriate message
+if alien_color == 'green':
+    print("Congratulations! You just earned yourself 5 points for shooting down a green alien.")
+elif alien_color == 'yellow':
+    print("Congratulations! You just earned yourself 10 points for shooting down a yellow alien.")
+else:
+    print("Congratulations! You just earned yourself 15 points for shooting down a red alien.")
