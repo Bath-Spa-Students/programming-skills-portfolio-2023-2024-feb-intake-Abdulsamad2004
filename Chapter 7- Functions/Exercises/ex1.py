a84a3fc8f2d62cd207c9dd915b171ad619826b7a
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:31:50 +0400

    Create ex1.py

---
+++ Chapter 7- Functions/Exercises/ex1.py
@@ -0,0 +1,7 @@
+def display_message():
+    """Prints a message about what you are learning in this chapter."""
+    message = "In this chapter, I am learning about using loops, dictionaries, and functions in Python."
+    print(message)
+
+# Call the function to display the message
+display_message()
