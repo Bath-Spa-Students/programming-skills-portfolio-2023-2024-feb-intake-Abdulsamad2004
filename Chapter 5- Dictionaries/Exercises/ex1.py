0faec7ddb7a45d6a4027219505bc56b00417c863
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:30:53 +0400

    Create ex1.py

---
+++ Chapter 5- Dictionaries/Exercises/ex1.py
@@ -0,0 +1,13 @@
+# Define a dictionary to store information about a person
+person = {
+    "first_name": "Abdul ",
+    "last_name": "Salam",
+    "age": 22,
+    "city": "Dubai"
+}
+
+# Print each piece of information stored in the dictionary
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
