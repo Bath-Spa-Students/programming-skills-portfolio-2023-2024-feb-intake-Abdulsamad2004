c6caf805594fc4a87ad8bdbd0fa519b4b6313a9e
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:31:59 +0400

    Create ex2.py

---
+++ Chapter 7- Functions/Exercises/ex2.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    """Prints a message indicating a favorite book."""
+    print(f"One of my favorite books is {title}.")
+
+# Call the function with a book title as an argument
+favorite_book("Hunger games")
