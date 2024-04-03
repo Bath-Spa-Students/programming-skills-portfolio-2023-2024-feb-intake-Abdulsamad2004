67ff19a3abb2e883cc7095556c4a85dbca4fc55b
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-03 11:32:11 +0400

    Create ex5.py

---
+++ Chapter 7- Functions/Exercises/ex5.py
@@ -0,0 +1,7 @@
+def describe_city(city, country='Unknown'):
+    print(f"{city} is in {country}.")
+
+# Calling the function for three different cities
+describe_city('Islamabad', 'Pakistan')
+describe_city('New York', 'USA')
+describe_city('Tokyo', 'Japan')
