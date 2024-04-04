ec3d0063a346c2b09abc57adb188c7ae56f411d3
Abdulsamad2004 <161293898+Abdulsamad2004@users.noreply.github.com>
2024-04-04 11:05:39 +0400

    Update ex4.py

---
+++ Chapter 4- Control Flow/Exercises/ex4.py
@@ -0,0 +1,16 @@
+# Set the value for the variable age
+age = 30
+
+# Determine the person's stage of life based on age
+if age < 2:
+    print("The person is a baby.")
+elif age < 4:
+    print("The person is a toddler.")
+elif age < 13:
+    print("The person is a kid.")
+elif age < 20:
+    print("The person is a teenager.")
+elif age < 65:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
